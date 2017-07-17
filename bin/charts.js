24a808ab045a93388df51d27029af37d8ed3c25c
liu fengyun <liu@fengy.me>
2017-07-17 12:02:11 +0200

    add deploy script

---
+++ bin/charts.js
@@ -0,0 +1,9 @@
+window.Bench = Bench || {}
+
+Bench.charts = [
+    {
+        "key": "tests/implicit_cache.scala",
+        "name": "implicit cache",
+        "url": "https://github.com/liufengyun/bench/blob/master/tests/implicit_cache.scala"
+    }
+]
