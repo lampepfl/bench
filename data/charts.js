a0c91cc48cbdb74b8578581bbd91a4f7790e97ab
Liu Fengyun <liu@fengy.me>
2017-07-17 09:24:55 +0200

    add initial commit for pages

---
+++ data/charts.js
@@ -0,0 +1,9 @@
+var Bench = Bench || {}
+
+Bench.charts = [
+    {
+        "key": "tests/implicit_cache.scala",
+        "name": "implicit cache",
+        "url": "https://github.com/liufengyun/bench/blob/master/tests/implicit_cache.scala"
+    }
+]
