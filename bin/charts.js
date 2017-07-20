87f5f11a51bca298b6e76c44f1aafa813257eb85
liu fengyun <liu@fengy.me>
2017-07-20 17:02:17 +0200

    add dotty to charts

--- bin/charts.js
+++ bin/charts.js
@@ -5,7 +5,13 @@ Bench.charts = [
         "key": "tests/implicit_cache.scala",
         "name": "implicit cache",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/implicit_cache.scala"
+    },
+    {
+        "key": "projects/dotty",
+        "name": "Dotty",
+        "url": "https://github.com/liufengyun/bench/blob/master/projects"
     }
+
 ]
 
 Bench.pr_base_url = "https://github.com/lampepfl/dotty/pull/"
