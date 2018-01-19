c677ac429e31b62e9a2300a8ee9c9e29baecfc08
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-01-19 10:46:05 +0100

    Add simple benchmark for macros

---
+++ tests/power-macro/PowerInlined-1k.scala
@@ -0,0 +1,13 @@
+
+object PowerInlined {
+  import PowerMacro._
+
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+}
