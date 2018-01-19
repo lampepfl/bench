adec720d8d3072a1f9ab371071ab1ec97ef3ffea
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-01-19 13:22:42 +0100

    Add missing charts

--- tests/power-macro/PowerMacro.scala
+++ tests/power-macro/PowerMacro.scala
@@ -4,12 +4,9 @@ object PowerMacro {
 
   inline def power(inline n: Long, x: Double) = ~powerCode(n, '(x))
 
-  def powerCode(n: Long, x: Expr[Double]): Expr[Double] = {
-    println(s"powerCode($n)")
+  def powerCode(n: Long, x: Expr[Double]): Expr[Double] =
     if (n == 0) '(1.0)
     else if (n % 2 == 0) '{ { val y = ~x * ~x; ~powerCode(n / 2, '(y)) } }
     else '{ ~x * ~powerCode(n - 1, x) }
-  }
-
 
 }
