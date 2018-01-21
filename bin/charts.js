82ac50cd413bf6397496189c3b13037128f65752
Liu Fengyun <liu@fengy.me>
2018-01-21 17:52:02 +0100

    support time series in display

--- bin/charts.js
+++ bin/charts.js
@@ -2,112 +2,97 @@ var Bench = Bench || {}
 
 Bench.charts = [
     {
-        "key": "dotty",
+        "lines": [ { "key": "dotty", "name": "bootstrapped" } ],
         "name": "dotty (source changes over time)",
         "url": "https://github.com/lampepfl/dotty"
     },
     {
-        "key": "tests/Vector.scala",
+        "lines": [ { "key": "tests/Vector.scala", "name": "bootstrapped" }, { "key": "opts/Vector.scala", "name": "optimised" } ],
         "name": "Vector",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/Vector.scala"
     },
     {
-        "key": "opts/Vector.scala",
-        "name": "Vector (optimised)",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/Vector.scala"
-    },
-    {
-        "key": "tests/scala-library",
+        "lines": [ { "key": "tests/scala-library", "name": "bootstrapped" }, { "key": "opts/scala-library", "name": "optimised" } ],
         "name": "scala library",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/scala-library"
     },
     {
-        "key": "opts/scala-library",
-        "name": "scala library (optimised)",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/scala-library"
-    },
-    {
-        "key": "tests/scalapb",
+        "lines": [ { "key": "tests/scalapb", "name": "bootstrapped" }, { "key": "opts/scalapb", "name": "optimised" } ],
         "name": "scalapb",
         "url": "https://github.com/liufengyun/ScalaPB/"
     },
     {
-        "key": "opts/scalapb",
-        "name": "scalapb (optimised)",
-        "url": "https://github.com/liufengyun/ScalaPB/"
-    },
-    {
-        "key": "tests/implicit_cache.scala",
+        "lines": [ { "key": "tests/implicit_cache.scala", "name": "bootstrapped" } ],
         "name": "implicit cache I",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/implicit_cache.scala"
     },
     {
-        "key": "tests/implicitNums.scala",
+        "lines": [ { "key": "tests/implicitNums.scala", "name": "bootstrapped" } ],
         "name": "implicit cache II",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/implicitNums.scala"
     },
     {
-        "key": "tests/implicit-scope-loop.scala",
+        "lines": [ { "key": "tests/implicit-scope-loop.scala", "name": "bootstrapped" } ],
         "name": "implicit scope loop",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/implicit-scope-loop.scala"
     },
     {
-        "key": "tests/patmatexhaust.scala",
+        "lines": [ { "key": "tests/patmatexhaust.scala", "name": "bootstrapped" } ],
         "name": "exhaustivity check",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/patmatexhaust.scala"
     },
     {
-        "key": "tests/exhaustivity-I.scala",
+        "lines": [ { "key": "tests/exhaustivity-I.scala", "name": "bootstrapped" } ],
         "name": "exhaustivity I",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-I.scala"
     },
     {
-        "key": "tests/exhaustivity-S.scala",
+        "lines": [ { "key": "tests/exhaustivity-S.scala", "name": "bootstrapped" } ],
         "name": "exhaustivity S",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-S.scala"
     },
     {
-        "key": "tests/exhaustivity-T.scala",
+        "lines": [ { "key": "tests/exhaustivity-T.scala", "name": "bootstrapped" } ],
         "name": "exhaustivity T",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-T.scala"
     },
     {
-        "key": "tests/exhaustivity-V.scala",
+        "lines": [ { "key": "tests/exhaustivity-V.scala", "name": "bootstrapped" } ],
         "name": "exhaustivity V",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-V.scala"
     },
     {
-        "key": "tests/i1535.scala",
+        "lines": [ { "key": "tests/i1535.scala", "name": "bootstrapped" } ],
         "name": "issue #1535",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/i1535.scala"
     },
     {
-        "key": "tests/i1687.scala",
+        "lines": [ { "key": "tests/i1687.scala", "name": "bootstrapped" } ],
         "name": "issue #1687",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/i1687.scala"
     },
     {
-        "key": "tests/empty-class.scala",
+        "lines": [ { "key": "tests/empty-class.scala", "name": "bootstrapped" } ],
         "name": "empty class",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/empty-class.scala"
     },
     {
-        "key": "tests/empty-object.scala",
+        "lines": [ { "key": "tests/empty-object.scala", "name": "bootstrapped" } ],
         "name": "empty object",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/empty-object.scala"
     },
     {
-        "key": "tests/empty-file.scala",
+        "lines": [ { "key": "tests/empty-file.scala", "name": "bootstrapped" } ],
         "name": "empty file",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/empty-file.scala"
     },
     {
-        "key": "tests/power-macro/PowerInlined-1.scala",
+        "lines": [ { "key": "tests/power-macro/PowerInlined-1.scala", "name": "bootstrapped" } ],
         "name": "Inline a quote",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/power-macro/PowerInlined-1.scala"
     },
     {
-        "key": "tests/power-macro/PowerInlined-1k.scala",
+        "lines": [ { "key": "tests/power-macro/PowerInlined-1k.scala", "name": "bootstrapped" } ],
         "name": "Inline 1k quotes",
         "url": "https://github.com/liufengyun/bench/blob/master/tests/power-macro/PowerInlined-1k.scala"
     }
