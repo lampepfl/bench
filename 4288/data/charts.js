a1268d1d0eefadaa5879c74b5c74859d07970a70
Liu Fengyun <liu@fengy.me>
2018-04-11 18:22:09 +0200

    [bot]update site for pull request 4288

--- 4288/data/charts.js
+++ 4288/data/charts.js
@@ -1,117 +1,25 @@
 var Bench = Bench || {}
-
 Bench.charts = [
-    {
-        "lines": [ { "key": "dotty", "name": "bootstrapped" } ],
-        "name": "dotty (source changes over time)",
-        "url": "https://github.com/lampepfl/dotty"
-    },
-    {
-        "lines": [ { "key": "vector", "name": "bootstrapped" },
-                   { "key": "vector-opt", "name": "optimised" }
-                 ],
-        "name": "Vector",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/Vector.scala"
-    },
-    {
-        "lines": [ { "key": "scala-library", "name": "bootstrapped" } ],
-        "name": "scala library",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/scala-library"
-    },
-    {
-        "lines": [ { "key": "scalapb", "name": "bootstrapped" } ],
-        "name": "scalapb",
-        "url": "https://github.com/liufengyun/ScalaPB/"
-    },
-    {
-        "lines": [ { "key": "scalap", "name": "bootstrapped" } ],
-        "name": "scalap",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/scalap"
-    },
-    {
-        "lines": [ { "key": "re2s", "name": "bootstrapped" } ],
-        "name": "re2",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/re2s"
-    },
-    {
-        "lines": [
-            { "key": "implicit-cache", "name": "bootstrapped" }
-        ],
-        "name": "implicit cache I",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/implicit_cache.scala"
-    },
-    {
-        "lines": [
-            { "key": "implicitNums", "name": "bootstrapped" }
-        ],
-        "name": "implicit cache II",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/implicitNums.scala"
-    },
-    {
-        "lines": [ { "key": "implicit-scope-loop", "name": "bootstrapped" } ],
-        "name": "implicit scope loop",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/implicit-scope-loop.scala"
-    },
-    {
-        "lines": [ { "key": "patmatexhaust", "name": "bootstrapped" } ],
-        "name": "exhaustivity check",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/patmatexhaust.scala"
-    },
-    {
-        "lines": [ { "key": "exhaustivity-I", "name": "bootstrapped" } ],
-        "name": "exhaustivity I",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-I.scala"
-    },
-    {
-        "lines": [ { "key": "exhaustivity-S", "name": "bootstrapped" } ],
-        "name": "exhaustivity S",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-S.scala"
-    },
-    {
-        "lines": [ { "key": "exhaustivity-T", "name": "bootstrapped" } ],
-        "name": "exhaustivity T",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-T.scala"
-    },
-    {
-        "lines": [ { "key": "exhaustivity-V", "name": "bootstrapped" } ],
-        "name": "exhaustivity V",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-V.scala"
-    },
-    {
-        "lines": [ { "key": "i1535", "name": "bootstrapped" } ],
-        "name": "issue #1535",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/i1535.scala"
-    },
-    {
-        "lines": [ { "key": "i1687", "name": "bootstrapped" } ],
-        "name": "issue #1687",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/i1687.scala"
-    },
-    {
-        "lines": [ { "key": "empty-class", "name": "bootstrapped" } ],
-        "name": "empty class",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/empty-class.scala"
-    },
-    {
-        "lines": [ { "key": "empty-object", "name": "bootstrapped" } ],
-        "name": "empty object",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/empty-object.scala"
-    },
-    {
-        "lines": [ { "key": "empty-file", "name": "bootstrapped" } ],
-        "name": "empty file",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/empty-file.scala"
-    },
-    {
-        "lines": [ { "key": "power-macro-power-inlined-1", "name": "bootstrapped" } ],
-        "name": "Inline a quote",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/power-macro/PowerInlined-1.scala"
-    },
-    {
-        "lines": [ { "key": "power-macro-power-inlined-1k", "name": "bootstrapped" } ],
-        "name": "Inline 1k quotes",
-        "url": "https://github.com/liufengyun/bench/blob/master/tests/power-macro/PowerInlined-1k.scala"
-    }
+{"name":"dotty (source changes over time)","url":"https://github.com/lampepfl/dotty","lines":[{"key":"dotty","label":"bootstrapped"}]},
+{"name":"Vector","url":"https://github.com/liufengyun/bench/blob/master/tests/Vector.scala","lines":[{"key":"vector","label":"bootstrapped"},{"key":"vector-opt","label":"optimised"}]},
+{"name":"scala library","url":"https://github.com/liufengyun/bench/blob/master/tests/scala-library","lines":[{"key":"scala-library","label":"bootstrapped"}]},
+{"name":"scalapb","url":"https://github.com/liufengyun/ScalaPB/","lines":[{"key":"scalapb","label":"bootstrapped"}]},
+{"name":"scalap","url":"https://github.com/liufengyun/bench/blob/master/tests/scalap","lines":[{"key":"scalap","label":"bootstrapped"}]},
+{"name":"re2","url":"https://github.com/liufengyun/bench/blob/master/tests/re2s","lines":[{"key":"re2s","label":"bootstrapped"}]},
+{"name":"implicit cache I","url":"https://github.com/liufengyun/bench/blob/master/tests/implicit_cache.scala","lines":[{"key":"implicit-cache","label":"bootstrapped"}]},
+{"name":"implicit cache II","url":"https://github.com/liufengyun/bench/blob/master/tests/implicitNums.scala","lines":[{"key":"implicitNums","label":"bootstrapped"}]},
+{"name":"implicit scope loop","url":"https://github.com/liufengyun/bench/blob/master/tests/implicit-scope-loop.scala","lines":[{"key":"implicit-scope-loop","label":"bootstrapped"}]},
+{"name":"exhaustivity check","url":"https://github.com/liufengyun/bench/blob/master/tests/patmatexhaust.scala","lines":[{"key":"patmatexhaust","label":"bootstrapped"}]},
+{"name":"exhaustivity I","url":"https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-I.scala","lines":[{"key":"exhaustivity-I","label":"bootstrapped"}]},
+{"name":"exhaustivity S","url":"https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-S.scala","lines":[{"key":"exhaustivity-S","label":"bootstrapped"}]},
+{"name":"exhaustivity T","url":"https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-T.scala","lines":[{"key":"exhaustivity-T","label":"bootstrapped"}]},
+{"name":"exhaustivity V","url":"https://github.com/liufengyun/bench/blob/master/tests/exhaustivity-V.scala","lines":[{"key":"exhaustivity-V","label":"bootstrapped"}]},
+{"name":"issue #1535","url":"https://github.com/liufengyun/bench/blob/master/tests/i1687.scala","lines":[{"key":"i1535","label":"bootstrapped"}]},
+{"name":"issue #1687","url":"https://github.com/liufengyun/bench/blob/master/tests/i1687.scala","lines":[{"key":"i1687","label":"bootstrapped"}]},
+{"name":"empty class","url":"https://github.com/liufengyun/bench/blob/master/tests/empty-class.scala","lines":[{"key":"empty-class","label":"bootstrapped"}]},
+{"name":"empty object","url":"https://github.com/liufengyun/bench/blob/master/tests/empty-object.scala","lines":[{"key":"empty-object","label":"bootstrapped"}]},
+{"name":"empty file","url":"https://github.com/liufengyun/bench/blob/master/tests/empty-file.scala","lines":[{"key":"empty-file","label":"bootstrapped"}]},
+{"name":"Inline a quote","url":"https://github.com/liufengyun/bench/blob/master/tests/power-macro/PowerInlined-1.scala","lines":[{"key":"power-macro-power-inlined-1","label":"bootstrapped"}]},
+{"name":"Inline 1k quotes","url":"https://github.com/liufengyun/bench/blob/master/tests/power-macro/PowerInlined-1k.scala","lines":[{"key":"power-macro-power-inlined-1k","label":"bootstrapped"}]}
 ]
-
-Bench.pr_base_url = "https://github.com/lampepfl/dotty/pull/"
+Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
