2a3c78f54fb64ab34b0f3ec4431557681cd6bcef
Liu Fengyun <liu@fengy.me>
2018-04-12 11:17:34 +0200

    [bot]update site for pull request 4298

---
+++ 4298/data/charts.js
@@ -0,0 +1,12 @@
+var Bench = Bench || {}
+Bench.charts = [
+{"name":"Vector","url":"https://github.com/liufengyun/bench/blob/master/tests/Vector.scala","lines":[{"key":"vector","label":"bootstrapped"}]},
+{"name":"scala library","url":"https://github.com/liufengyun/bench/blob/master/tests/scala-library","lines":[{"key":"scala-library","label":"bootstrapped"}]},
+{"name":"scalapb","url":"https://github.com/liufengyun/ScalaPB/","lines":[{"key":"scalapb","label":"bootstrapped"}]},
+{"name":"scalap","url":"https://github.com/liufengyun/bench/blob/master/tests/scalap","lines":[{"key":"scalap","label":"bootstrapped"}]},
+{"name":"re2","url":"https://github.com/liufengyun/bench/blob/master/tests/re2s","lines":[{"key":"re2s","label":"bootstrapped"}]},
+{"name":"empty class","url":"https://github.com/liufengyun/bench/blob/master/tests/empty-class.scala","lines":[{"key":"empty-class","label":"bootstrapped"}]},
+{"name":"empty object","url":"https://github.com/liufengyun/bench/blob/master/tests/empty-object.scala","lines":[{"key":"empty-object","label":"bootstrapped"}]},
+{"name":"empty file","url":"https://github.com/liufengyun/bench/blob/master/tests/empty-file.scala","lines":[{"key":"empty-file","label":"bootstrapped"}]}
+]
+Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
