c01ea2c382d9e3a5a5c986256c5fccdad7412e4e
Liu Fengyun <liu@fengy.me>
2018-08-14 12:09:25 +0200

    also add generated plans/js to source control

---
+++ profiles/sbt.js
@@ -0,0 +1,5 @@
+var Bench = Bench || {}
+Bench.charts = [
+{"name":"dotty (source changes over time)","url":"https://github.com/lampepfl/dotty","lines":[{"key":"dotty","label":"bootstrapped"},{"key":"dotty-sbt","label":"with sbt phases"}]}
+]
+Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
---
+++ profiles/tasty.js
@@ -0,0 +1,5 @@
+var Bench = Bench || {}
+Bench.charts = [
+{"name":"Vector","url":"https://github.com/liufengyun/bench/blob/master/tests/Vector.scala","lines":[{"key":"vector-from-tasty","label":"from tasty"}]}
+]
+Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
