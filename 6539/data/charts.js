171f2954a92ae1a855d3e47b78736ebc4f3f415a
Liu Fengyun <liu@fengy.me>
2019-05-21 09:18:59 +0200

    [bot]update site for pull request 6539

---
+++ 6539/data/charts.js
@@ -0,0 +1,6 @@
+var Bench = Bench || {}
+Bench.charts = [
+{"name":"Tuple22.tail","url":"https://github.com/liufengyun/bench/blob/master/tests/tuple22-tail.scala","lines":[{"key":"tuple22-tail","label":"bootstrapped"}]},
+{"name":"Generic Tuple22.tail","url":"https://github.com/liufengyun/bench/blob/master/tests/generic-tuple22-tail.scala","lines":[{"key":"generic-tuple22-tail","label":"bootstrapped"}]}
+]
+Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
