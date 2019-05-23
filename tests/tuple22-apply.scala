b118388c5983dd26ea6f694f03408d71ec2c232a
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-05-23 09:02:16 +0200

    Add better Tuple benchmarks

---
+++ tests/tuple22-apply.scala
@@ -0,0 +1,8 @@
+class Foo {
+  val tup: (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) = ???
+  tup.apply(0)
+  tup.apply(5)
+  tup.apply(10)
+  tup.apply(15)
+  tup.apply(21)
+}
---
+++ tests/tuple22-tails.scala
@@ -0,0 +1,4 @@
+class Foo {
+  val tup: (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) = ???
+  tup.tail.tail.tail.tail.tail
+}
