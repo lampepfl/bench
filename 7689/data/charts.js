7cac4ec440166c9908b9e0735ef619269a824338
Liu Fengyun <liu@fengy.me>
2019-12-10 09:29:09 +0100

    [bot]update site for pull request 7689

---
+++ 7689/data/charts.js
@@ -0,0 +1,5 @@
+var Bench = Bench || {}
+Bench.charts = [
+{"name":"Tuple reverse","lines":[{"key":"tuple-reverse","label":"bootstrapped"}]},
+{"name":"Tuple flatMap","lines":[{"key":"tuple-flatMap","label":"bootstrapped"}]}
+]
