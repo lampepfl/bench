2acb4ef7ced063f4a9ec5a0269ca9a1fb528d503
Liu Fengyun <liu@fengy.me>
2019-12-17 16:18:55 +0100

    Add runtime benchmarks to default profile

--- profiles/default.js
+++ profiles/default.js
@@ -24,6 +24,8 @@ Bench.charts = [
 {"name":"Tuple22 creation with *:","url":"https://github.com/lampepfl/bench/blob/master/tests/tuple22-creation-cons.scala","lines":[{"key":"tuple22-creation-cons","label":"bootstrapped"}]},
 {"name":"Tuple22.tail","url":"https://github.com/lampepfl/bench/blob/master/tests/tuple22-tails.scala","lines":[{"key":"tuple22-tails","label":"bootstrapped"}]},
 {"name":"Tuple22.apply","url":"https://github.com/lampepfl/bench/blob/master/tests/tuple22-apply.scala","lines":[{"key":"tuple22-apply","label":"bootstrapped"}]},
-{"name":"Tuple22.size","url":"https://github.com/lampepfl/bench/blob/master/tests/tuple22-size.scala","lines":[{"key":"tuple22-size","label":"bootstrapped"}]}
+{"name":"Tuple22.size","url":"https://github.com/lampepfl/bench/blob/master/tests/tuple22-size.scala","lines":[{"key":"tuple22-size","label":"bootstrapped"}]},
+{"name":"Tuple reverse","lines":[{"key":"tuple-reverse","label":"bootstrapped"}]},
+{"name":"Tuple flatMap","lines":[{"key":"tuple-flatMap","label":"bootstrapped"}]}
 ]
 Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
