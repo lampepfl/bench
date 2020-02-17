19b6f7a60164d13b687ce963c0f1a1289ad39675
Liu Fengyun <liu@fengy.me>
2020-02-17 10:15:38 +0100

    [bot]update site for pull request 8326

---
+++ 8326/data/charts.js
@@ -0,0 +1,24 @@
+var Bench = Bench || {}
+Bench.charts = [
+{"name":"dotty (source changes over time)","url":"https://github.com/lampepfl/dotty","lines":[{"key":"dotty","label":"bootstrapped"}]},
+{"name":"scala stdlib-2.13","url":"https://github.com/dotty-staging/scala/commits/stdLib213-dotty-community-build","lines":[{"key":"stdlib213","label":"bootstrapped"}]},
+{"name":"scalap","url":"https://github.com/dotty-staging/scala/commits/scalap-dotty-community-build-2.13","lines":[{"key":"scalap","label":"bootstrapped"}]},
+{"name":"re2","url":"https://github.com/lampepfl/bench/blob/master/tests/re2s","lines":[{"key":"re2s","label":"bootstrapped"}]},
+{"name":"implicit cache I","url":"https://github.com/lampepfl/bench/blob/master/tests/implicit_cache.scala","lines":[{"key":"implicit-cache","label":"bootstrapped"}]},
+{"name":"implicit cache II","url":"https://github.com/lampepfl/bench/blob/master/tests/implicitNums.scala","lines":[{"key":"implicitNums","label":"bootstrapped"}]},
+{"name":"implicit scope loop","url":"https://github.com/lampepfl/bench/blob/master/tests/implicit-scope-loop.scala","lines":[{"key":"implicit-scope-loop","label":"bootstrapped"}]},
+{"name":"inductive implicits","url":"https://github.com/lampepfl/bench/blob/master/tests/inductive-implicits.scala","lines":[{"key":"inductive-implicits","label":"bootstrapped"}]},
+{"name":"exhaustivity check","url":"https://github.com/lampepfl/bench/blob/master/tests/patmatexhaust.scala","lines":[{"key":"patmatexhaust","label":"bootstrapped"}]},
+{"name":"exhaustivity I","url":"https://github.com/lampepfl/bench/blob/master/tests/exhaustivity-I.scala","lines":[{"key":"exhaustivity-I","label":"bootstrapped"}]},
+{"name":"exhaustivity S","url":"https://github.com/lampepfl/bench/blob/master/tests/exhaustivity-S.scala","lines":[{"key":"exhaustivity-S","label":"bootstrapped"}]},
+{"name":"exhaustivity T","url":"https://github.com/lampepfl/bench/blob/master/tests/exhaustivity-T.scala","lines":[{"key":"exhaustivity-T","label":"bootstrapped"}]},
+{"name":"exhaustivity V","url":"https://github.com/lampepfl/bench/blob/master/tests/exhaustivity-V.scala","lines":[{"key":"exhaustivity-V","label":"bootstrapped"}]},
+{"name":"issue #1535","url":"https://github.com/lampepfl/bench/blob/master/tests/i535.scala","lines":[{"key":"i1535","label":"bootstrapped"}]},
+{"name":"issue #1687","url":"https://github.com/lampepfl/bench/blob/master/tests/i1687.scala","lines":[{"key":"i1687","label":"bootstrapped"}]},
+{"name":"empty class","url":"https://github.com/lampepfl/bench/blob/master/tests/empty-class.scala","lines":[{"key":"empty-class","label":"bootstrapped"}]},
+{"name":"empty object","url":"https://github.com/lampepfl/bench/blob/master/tests/empty-object.scala","lines":[{"key":"empty-object","label":"bootstrapped"}]},
+{"name":"empty file","url":"https://github.com/lampepfl/bench/blob/master/tests/empty-file.scala","lines":[{"key":"empty-file","label":"bootstrapped"}]},
+{"name":"Inline a quote","url":"https://github.com/lampepfl/bench/blob/master/tests/power-macro/PowerInlined-1.scala","lines":[{"key":"power-macro-power-inlined-1","label":"bootstrapped"}]},
+{"name":"Inline 1k quotes","url":"https://github.com/lampepfl/bench/blob/master/tests/power-macro/PowerInlined-1k.scala","lines":[{"key":"power-macro-power-inlined-1k","label":"bootstrapped"}]}
+]
+Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
---
+++ 8326/main.js
@@ -0,0 +1,347 @@
+var Bench = Bench || {};
+
+function process(item) {
+  return { y: item[3], x: item[1], obj: item };
+}
+
+function sort(points) {
+  return points.sort(function(a, b) {
+    return new Date(a.obj[1]) - new Date(b.obj[1]);
+  });
+}
+
+function sample(points, rate) {
+  return points.reduce(function(acc, item) {
+    if (Math.random() < rate) acc.push(item);
+
+    return acc;
+  }, []);
+}
+
+function dedup(points) {
+  var lastDate = null;
+  points = points.reduce(function(acc, point) {
+    var curDate = new Date(point.x).toLocaleDateString();
+    if (!lastDate || lastDate !== curDate) {
+      lastDate = curDate;
+      acc.push(point);
+    }
+    return acc;
+  }, []);
+
+  return points;
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function flatten() {
+  Bench.flattened = [];
+
+  Bench.charts.map(function(chart) {
+    chart.lines.map(function(line) {
+      var name = chart.name;
+      if (chart.lines.length > 1) name = name + " - " + line.label;
+      Bench.flattened.push({
+        "key": line.key,
+        "name": name,
+        "url": chart.url
+      });
+    });
+  });
+}
+
+function ChartView(chart, dom) {
+  var view = {
+    // checkChanged: fn,   // register event
+    dom: dom,
+    chart: chart,
+    data: {},
+    options: {},
+    ready: false,
+    showAll: false,
+    render: render
+  };
+
+  function render() {
+    var width = $("#app").width(); // window size may change
+
+    if (view.ready) {
+      $("input.show-all", dom).off("change"); // avoid memory leaks
+      dom.innerHTML =
+        `<div>
+          <h3>
+            <a href=${view.chart.url}>${view.chart.name}</a>
+            <span style="float: right">
+              <input type="checkbox" class="show-all" ${view.showAll ? "checked" : ""}/>
+              <small style="color: #aaa; fontSize: 15px"> show all points</small>
+            </span>
+          </h3>
+          <canvas width=${width} height="300"></canvas>
+        </div>`
+
+      var ctx = $('canvas', dom)[0].getContext('2d');
+      var lineChart = new Chart(ctx, {
+        type: "line",
+        data: view.data,
+        options: view.options
+      });
+
+      $("input.show-all", dom).on("change", e => {
+        view.checkChanged(e);
+      });
+    }
+    else {
+      dom.innerHTML = `<div><h2>${view.chart.name}</h2><p>Loading...</p></div>`
+    }
+  }
+
+  render();
+
+  return view;
+};
+
+function showChartList(charts, dataProvider, optionProvider) {
+  var container = document.getElementById('app');
+  container.innerHTML = "";
+  charts.map(function (chart) {
+    var dom = document.createElement("div");
+    container.appendChild(dom);
+    var view = ChartView(chart, dom);
+
+    function load(showAll) {
+      dataProvider(chart, !showAll, function(data) {
+        view.data = data;
+        view.ready = true;
+        view.options = optionProvider(view);
+        view.showAll = showAll;
+        view.render();
+      })
+    }
+
+    view.checkChanged = function(e) {
+      load(e.target.checked);
+    }
+
+    load(false);
+  });
+}
+
+window.showTime = function() {
+  var colorNames  = ['yellow', 'purple', 'orange', 'green', 'blue', 'red', 'grey'];
+  var colors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+  };
+
+  function getData(chart, isDedup, callback) {
+    var dataset = []
+    var deferreds = chart.lines.map(function(line) {
+      return $.get("data/" + line.key + ".json", function(data) {
+        dataset.push({ line: line, data: data });
+      })
+    })
+
+    $.when.apply($, deferreds).then(function() {
+      callback({ datasets: prepareData(dataset, isDedup), labels: [] });
+    })
+  }
+
+  function prepareData(datasets, isDedup) {
+    var index = -1;
+    return datasets.map(function(tuple) {
+      index++;
+
+      var points = sort(tuple.data.map(process))
+      if (isDedup) points = dedup(points);
+
+      return {
+        label: tuple.line.label,
+        fill: false,
+        backgroundColor: colors[colorNames[index]],
+        data: points
+      };
+    });
+  }
+
+  function options(view) {
+    function getItem(didx, pidx) {
+      return view.data["datasets"][didx].data[pidx].obj;
+    }
+
+    return {
+      responsive: true,
+      legend: {
+        display: view.chart.lines.length > 1
+      },
+      tooltips: {
+        callbacks: {
+          title: function (data) {
+            var item = getItem(data[0].datasetIndex, data[0].index);
+            var date = new Date(item[1]);
+            var day = date.getDate();
+            var month = date.getMonth() + 1;
+            var year = date.getFullYear();
+            return "PR#" + item[0] + " \n" + item[2] + " \n" + day + "/" + month + "/" + year;
+          },
+          label: function (point) {
+            var item = getItem(point.datasetIndex, point.index);
+            return numberWithCommas(point.yLabel) + "ms";
+          }
+        },
+        mode: "index",
+        intersect: false
+      },
+      scales: {
+        xAxes: [{
+          type: "time",
+          time: {
+            // format: 'MM/DD/YYYY HH:mm',
+            // round: 'day'
+            tooltipFormat: 'll HH:mm'
+          },
+          scaleLabel: {
+            display: false
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: { display: true, labelString: 'ms' },
+          ticks: {
+            min: 0,
+            callback: function(value, index, values) { return numberWithCommas(value); }
+          }
+        }]
+      },
+      onClick: function(e) {
+        var activeElems = this.getElementsAtEvent(e);
+        if (activeElems.length > 0) {
+          var pindex = activeElems[0]._index;
+          var dindex = activeElems[0]._datasetIndex;
+          var obj = getItem(dindex, pindex);
+          var win = window.open(Bench.config.pr_base_url + obj[0], '_blank');
+          win.focus();
+        }
+      }
+    }
+  }
+
+  showChartList(Bench.charts, getData, options);
+}
+
+window.showCommit = function () {
+  function getData(chart, isSample, callback) {
+    $.get("data/" + chart.key + ".json", function (data) { // data cached by browser
+      callback(prepareData(data, isSample))
+    })
+  }
+
+  function prepareData(data, isSample) {
+    var points = sort(data.map(process))
+
+    if (isSample) {
+      var pts1 = sample(points.slice(0, -100), 50 / (points.length - 100));
+      var pts2 = sample(points.slice(-100, -40), 0.5);
+      var pts3 = points.slice(-40);
+
+      points = pts1.concat(pts2).concat(pts3);
+    }
+
+    var median = {
+        label: "median",
+        data: points,
+        fill: false,
+        borderColor: "rgba(100,100,100,0.2)",
+        backgroundColor: "yellow",
+        pointBorderColor: "rgba(220,220,220,1)",
+        pointBackgroundColor: "yellow",
+        pointBorderWidth: 1
+    }
+
+    var labels = [];
+    for (var i = 0; i < points.length; i++) {
+      var date = new Date(points[i].obj[1]);
+      var day = date.getDate();
+      var month = date.getMonth() + 1;
+      labels.push(day + "/" + month);
+    }
+
+    var minPoints = points.map(function(p) {
+      return { y: p.obj[4], x: p.index, obj: p.obj }
+    });
+
+    var min = {
+        label: "min",
+        data: minPoints,
+        fill: false,
+        borderDash: [5, 5],
+        pointBorderWidth: 1
+    }
+
+    return {
+      labels: labels,
+      datasets: [median, min]
+    }
+  }
+
+  function options(view) {
+    function getItem(didx, pidx) {
+      return view.data["datasets"][didx].data[pidx].obj;
+    }
+
+    return {
+      responsive: true,
+      tooltips: {
+        callbacks: {
+          title: function (data) {
+            var item = getItem(data[0].datasetIndex, data[0].index);
+            var date = new Date(item[1]);
+            var day = date.getDate();
+            var month = date.getMonth() + 1;
+            var year = date.getFullYear();
+            return "PR#" + item[0] + " \n" + item[2] + " \n" + day + "/" + month + "/" + year;
+          },
+          label: function (point) {
+            var item = getItem(point.datasetIndex, point.index);
+            return numberWithCommas(point.yLabel) + "ms";
+          }
+        },
+        mode: "index",
+        intersect: false
+      },
+      scales: {
+        yAxes: [{
+          display: true,
+          scaleLabel: { display: true, labelString: 'ms' },
+          ticks: {
+            min: 0,
+            callback: function(value, index, values) { return numberWithCommas(value); }
+          }
+        }]
+      },
+      onClick: function(e) {
+        var activeElems = this.getElementsAtEvent(e);
+        if (activeElems.length > 0) {
+          var pindex = activeElems[0]._index;
+          var dindex = activeElems[0]._datasetIndex;
+          var obj = getItem(dindex, pindex);
+          var win = window.open(Bench.config.pr_base_url + obj[0], '_blank');
+          win.focus();
+        }
+      }
+    }
+  }
+
+  showChartList(Bench.flattened, getData, options);
+}
+
+$(function () {
+  flatten();
+  showCommit();
+})
