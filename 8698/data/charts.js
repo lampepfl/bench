8edff0385e7dfb2b71ea12a25bd2528ce1399cb4
Liu Fengyun <liu@fengy.me>
2020-04-09 17:40:30 +0200

    [bot]update site for pull request 8698

---
+++ 8698/data/charts.js
@@ -0,0 +1,9 @@
+var Bench = Bench || {}
+Bench.charts = [
+{"name":"scalap","url":"https://github.com/lampepfl/bench/blob/master/tests/scalap","lines":[{"key":"scalap","label":"bootstrapped"}]},
+{"name":"re2","url":"https://github.com/lampepfl/bench/blob/master/tests/re2s","lines":[{"key":"re2s","label":"bootstrapped"}]},
+{"name":"empty class","url":"https://github.com/lampepfl/bench/blob/master/tests/empty-class.scala","lines":[{"key":"empty-class","label":"bootstrapped"}]},
+{"name":"empty object","url":"https://github.com/lampepfl/bench/blob/master/tests/empty-object.scala","lines":[{"key":"empty-object","label":"bootstrapped"}]},
+{"name":"empty file","url":"https://github.com/lampepfl/bench/blob/master/tests/empty-file.scala","lines":[{"key":"empty-file","label":"bootstrapped"}]}
+]
+Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
