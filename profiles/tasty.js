497a08ad3e6239854939da14be02e985686f9d19
Liu Fengyun <liu@fengy.me>
2020-08-25 10:30:12 +0200

    Synchronize files

--- profiles/tasty.js
+++ profiles/tasty.js
@@ -1,6 +1,6 @@
 var Bench = Bench || {}
 Bench.charts = [
-{"name":"implicit cache I","url":"https://github.com/lampepfl/bench/blob/master/tests/implicit_cache.scala","lines":[{"key":"implicit-cache","label":"bootstrapped"},{"key":"implicit-cache-from-tasty","label":"from tasty"}]},
-{"name":"implicit cache II","url":"https://github.com/lampepfl/bench/blob/master/tests/implicitNums.scala","lines":[{"key":"implicitNums","label":"bootstrapped"},{"key":"implicitNums-from-tasty","label":"from tasty"}]}
+{"name":"implicit cache I","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/implicit_cache.scala","lines":[{"key":"implicit-cache","label":"bootstrapped"},{"key":"implicit-cache-from-tasty","label":"from tasty"}]},
+{"name":"implicit cache II","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/implicitNums.scala","lines":[{"key":"implicitNums","label":"bootstrapped"},{"key":"implicitNums-from-tasty","label":"from tasty"}]}
 ]
 Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
