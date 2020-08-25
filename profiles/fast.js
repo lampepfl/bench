8299c05ec6a26e940da8c8f5ee4d9bfc5b14ded1
Liu Fengyun <liu@fengy.me>
2020-08-25 13:53:21 +0200

    Remove obsolete profiles

--- profiles/fast.js
+++
@@ -1,9 +0,0 @@
-var Bench = Bench || {}
-Bench.charts = [
-{"name":"scalap","url":"https://github.com/lampepfl/bench/blob/master/tests/scalap","lines":[{"key":"scalap","label":"bootstrapped"}]},
-{"name":"re2","url":"https://github.com/lampepfl/bench/blob/master/tests/re2s","lines":[{"key":"re2s","label":"bootstrapped"}]},
-{"name":"empty class","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/empty-class.scala","lines":[{"key":"empty-class","label":"bootstrapped"}]},
-{"name":"empty object","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/empty-object.scala","lines":[{"key":"empty-object","label":"bootstrapped"}]},
-{"name":"empty file","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/empty-file.scala","lines":[{"key":"empty-file","label":"bootstrapped"}]}
-]
-Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
