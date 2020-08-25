4a7db813c92e8ce3944b6408c372142795c90b47
Liu Fengyun <liu@fengy.me>
2020-08-25 10:30:53 +0200

    Remove obsolete scala files

--- tests/power-macro/PowerInlined-1.scala
+++
@@ -1,6 +0,0 @@
-
-object PowerInlined {
-  import PowerMacro._
-
-  power(1, 5.0) // 1 quotes to unpickle
-}
--- tests/tuple22-size.scala
+++
@@ -1,8 +0,0 @@
-class Foo {
-  val tup: (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) = ???
-  tup.size
-  tup.size
-  tup.size
-  tup.size
-  tup.size
-}
