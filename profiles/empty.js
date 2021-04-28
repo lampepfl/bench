4d6c476bc56cdf0f3af12fda134dd987b054f970
Liu Fengyun <liu@fengy.me>
2021-04-28 13:57:07 +0200

    Use profiles from Dotty

--- profiles/empty.js
+++
@@ -1,7 +0,0 @@
-var Bench = Bench || {}
-Bench.charts = [
-{"name":"empty class","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/empty-class.scala","lines":[{"key":"empty-class","label":"bootstrapped"}]},
-{"name":"empty object","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/empty-object.scala","lines":[{"key":"empty-object","label":"bootstrapped"}]},
-{"name":"empty file","url":"https://github.com/lampepfl/dotty/blob/master/tests/bench/empty-file.scala","lines":[{"key":"empty-file","label":"bootstrapped"}]}
-]
-Bench.config = {"pr_base_url":"https://github.com/lampepfl/dotty/pull/"}
